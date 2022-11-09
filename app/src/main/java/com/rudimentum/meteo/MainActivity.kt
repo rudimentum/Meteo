bfb7f96839b1176c8a5daeb23c0051bfd69e0f29
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-11-10 00:12:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/rudimentum/meteo/MainActivity.kt
+++ app/src/main/java/com/rudimentum/meteo/MainActivity.kt
@@ -2,8 +2,13 @@ package com.rudimentum.meteo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import com.rudimentum.meteo.databinding.ActivityMainBinding
-
+import com.rudimentum.meteo.screens.today.TodayWeatherFragment
+import com.rudimentum.meteo.screens.week.WeekForecastFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,33 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val todayFragment = TodayWeatherFragment()
+        val weekFragment = WeekForecastFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayoutFragment, todayFragment)
+            commit()
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener() { item ->
+            when(item.itemId) {
+                R.id.today -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.frameLayoutFragment, todayFragment)
+                        commit()
+                    }
+                    true
+                }
+                R.id.week -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.frameLayoutFragment, weekFragment)
+                        commit()
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onDestroy() {
