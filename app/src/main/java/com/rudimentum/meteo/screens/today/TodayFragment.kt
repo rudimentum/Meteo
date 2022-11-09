ae9232627f84c334f53b6865904847d3bdcff62a
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-11-09 13:06:37 +0300

    Add permissions
    Add permissions in Manifest, implement requesting and checking ACCESS_FINE_LOCATION permission.

--- app/src/main/java/com/rudimentum/meteo/screens/today/TodayFragment.kt
+++ app/src/main/java/com/rudimentum/meteo/screens/today/TodayFragment.kt
@@ -5,20 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rudimentum.meteo.R
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.rudimentum.meteo.databinding.FragmentTodayBinding
+import com.rudimentum.meteo.utils.isPermissionGranted
 
 class TodayFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
+    private var _binding: FragmentTodayBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today, container, false)
+        _binding = FragmentTodayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        permissionCheck()
+    }
+
+    private fun permissionListener() {
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun permissionCheck() {
+        if (!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
 }
---
+++ app/src/main/java/com/rudimentum/meteo/utils/Extensions.kt
@@ -0,0 +1,12 @@
+package com.rudimentum.meteo.utils
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.isPermissionGranted(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        activity as AppCompatActivity,
+        permission) == PackageManager.PERMISSION_GRANTED
+}
