e6860e012007a5d92ceeee7e086ec2cf2c73f934
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-11-10 00:12:07 +0300

    Add Constants

---
+++ app/src/main/java/com/rudimentum/meteo/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.rudimentum.meteo.utils
+
+const val BASE_URL = "https://api.weatherapi.com/v1/"
+const val WEEK = "7"
+const val ONE_DAY = "1"
+const val API_KEY = "53d683deddc34586acc190430220811"
+const val AQI = "no"
+const val ALERTS = "no"
+var Q: String = "Moscow"
